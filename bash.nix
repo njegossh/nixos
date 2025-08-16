420cd916497dc55415383cf981d05e7914252a93
markopetrovic <markopetrovic3939@gmail.com>
2025-08-16 23:18:39 +0200

    Bigger bash history

--- bash.nix
+++ bash.nix
@@ -1,7 +1,14 @@
 { ... } : {
-  programs.bash.promptInit = ''
-    PS1="[ \w ] \$\[\033[0m\] "
-    eval "$(fzf --bash)"
-    bind -x '"\C-p": __fzf_history__'
-  '';
+  programs.bash = {
+    shellInit = ''
+      export HISTSIZE=5000
+      export HISTFILESIZE=10000
+      export HISTCONTROL=ignorespace
+    '';
+    promptInit = ''
+      PS1="[ \w ] \$\[\033[0m\] "
+      eval "$(fzf --bash)"
+      bind -x '"\C-p": __fzf_history__'
+    '';
+  };
 }
