0527ff62b865b298433d4b07e7329f404425c8ff
markopetrovic <markopetrovic3939@gmail.com>
2025-09-18 17:02:19 +0200

    Terminal

--- gnome.nix
+++ gnome.nix
@@ -37,7 +37,7 @@
         ];
         "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
           name = "Terminal";
-          command = "kgx";
+          command = "blackbox";
           binding = "<Super>Return";
         };
         "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
