b9fc98a80a1eb1ad25e6a5e92797881511def33f
Marko Petrovic <markopetrovic3939@gmail.com>
2025-10-17 22:50:38 +0200

    Wireguard

--- network.nix
+++ network.nix
@@ -2,20 +2,32 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
+
+    firewall = {
+      allowedTCPPorts = [ 
+        4444 7657 #I2P 
+        22000     #Syncthing 
+        42410     #I2P Custom
+        51820     #Wireguard
+      ];
+      allowedUDPPorts = [ 
+        21027     #Syncthing 
+        42410     #I2P Custom
+      ];
+    };
+
+    wireguard = {
+      enable = true;
+      interfaces = {
+        wg0 = {
+          privateKeyFile = "/etc/wireguard/server_private.key";
+          listenPort = 51820;
+        };
+      };
+    };
   };
+
   services.i2p.enable = true;
-
-  networking.firewall.allowedTCPPorts = [ 
-    4444 7657 #I2P 
-    22000     #Syncthing 
-    42410     #I2P Custom
-  ];
-
-  networking.firewall.allowedUDPPorts = [ 
-    21027     #Syncthing 
-    42410     #I2P Custom
-  ];
-
   environment.etc."i2p/router.config".text = ''
     i2np.udp.internalPort=42410
     i2np.udp.port=42410
