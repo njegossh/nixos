be6d921296ce3b293b1c2dec843b8882f9d81dbf
Marko Petrovic <markopetrovic3939@gmail.com>
2025-10-31 17:51:37 +0100

    Changes

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
           Shop =      { color = "green";    icon = "cart";        id = 3; };
           Uni =       { color = "purple";   icon = "fence";       id = 4; };
           I2P =       { color = "toolbar";  icon = "chill";       id = 5; };
+          Business =  { color = "yellow";   icon = "briefcase";   id = 6; };
         };
       };
     };
