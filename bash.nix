51de3ed45a1d0faa007ebf7e39cb74e22635c4b8
Marko Petrovic <markopetrovic3939@gmail.com>
2025-11-02 17:38:26 +0100

    Bash changes

--- bash.nix
+++ bash.nix
@@ -1,25 +1,33 @@
-{ ... } : {
+{ ... }:
+let
+  bashCommons = ''
+    sudo du -sh *                                                           #Scan disk usage for current folder
+    sudo nix flake update                                                   #NixOS update
+    sudo docker start vpn && docker exec -it vpn bash && docker stop vpn    #Docker BarracudaVPN
+    git log --patch --stat                                                  #All commits
+    git diff                                                                #See uncommited changes
+    sudo nixos-rebuild switch --upgrade --flake .#intel                     #NixOS rebuild Intel
+  '';
+in {
   programs.bash = {
     shellInit = ''
       export HISTSIZE=5000
       export HISTFILESIZE=10000
       export HISTCONTROL=ignorespace
-
-      _fzf_custom_search() {
-        local header_opts="--header='History (Ctrl-R) | Commands (Ctrl-B)'"
-        
-        ( history | sed 's/^[ ]*[0-9]*[ ]*//' ; cat "/home/marko/Documents/Sync/Quick.txt" ) | \
-        FZF_DEFAULT_OPTS="$header_opts" fzf \
-          --reverse --height 50% --no-sort \
-          --bind "enter:replace-line"
-          
-      }
-      bind -x '"\C-b": _fzf_custom_search'
     '';
     promptInit = ''
       PS1="[ \w ] \$\[\033[0m\] "
-      eval "$(fzf --bash)"
-      bind -x '"\C-p": __fzf_history__'
+
+      _fzf_custom_search() {
+        local hist=$(history | sed 's/^[ ]*[0-9]*[ ]*//')
+        local inputs=$(echo "$hist" ; echo "${bashCommons}")
+        local unique=$(echo "$inputs" | awk '!seen[$0]++')
+        READLINE_LINE=$(echo "$unique" | fzf --tac) || return
+
+        READLINE_POINT=''${#READLINE_LINE}
+      }
+
+      bind -x '"\C-p": _fzf_custom_search'
     '';
   };
 }
