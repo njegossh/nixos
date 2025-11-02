a602ad3eb330c2783b24739f0c130883c204f10b
Marko Petrovic <markopetrovic3939@gmail.com>
2025-11-02 13:54:22 +0100

    Lock NVF to PR

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		nvf.url = "github:notashelf/nvf";
+		nvf.url = "github:notashelf/nvf/pull/1209/head";
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
   outputs = { home-manager, nix-flatpak, nixpkgs, nvf, ... } : let
