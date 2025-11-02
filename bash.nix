d14f8a102527174494a55f797f0937f3769791c0
Marko Petrovic <markopetrovic3939@gmail.com>
2025-11-02 18:46:59 +0100

    Bashed

--- bash.nix
+++ bash.nix
@@ -1,13 +1,18 @@
-{ ... }:
-let
-  bashCommons = ''
-    sudo du -sh *                                                           #Scan disk usage for current folder
-    sudo nix flake update                                                   #NixOS update
-    sudo docker start vpn && docker exec -it vpn bash && docker stop vpn    #Docker BarracudaVPN
-    git log --patch --stat                                                  #All commits
-    git diff                                                                #See uncommited changes
-    sudo nixos-rebuild switch --upgrade --flake .#intel                     #NixOS rebuild Intel
-  '';
+{ config, ... } : let
+  home = config.users.users.marko.home;
+  work = "cd ${home}/Documents/Code/Gitea";
+  berg = "cd ${home}/Documents/Code/Codeberg";
+  commons = [
+    "########################################"
+    "################Directory###############"
+    "${berg}/NixOS"     "${berg}/Boardline" "${berg}/Template"
+    "${work}/Invoicing" "${work}/Expenses"  "${work}/Commons"
+    "################Commands################"
+    "sudo du -sh *"
+    "sudo docker start vpn && docker exec -it vpn bash && docker stop vpn"
+    "git log --patch --stat" "git diff" "git add . && git commit -m"
+    "sudo nix flake update" "sudo nixos-rebuild switch --upgrade --flake .#intel"
+  ];
 in {
   programs.bash = {
     shellInit = ''
@@ -18,16 +23,18 @@ in {
     promptInit = ''
       PS1="[ \w ] \$\[\033[0m\] "
 
-      _fzf_custom_search() {
-        local hist=$(history | sed 's/^[ ]*[0-9]*[ ]*//')
-        local inputs=$(echo "$hist" ; echo "${bashCommons}")
-        local unique=$(echo "$inputs" | awk '!seen[$0]++')
-        READLINE_LINE=$(echo "$unique" | fzf --tac) || return
+      commons=(${builtins.concatStringsSep "\n        " (map (c: ''"${c}"'') commons)})
 
+      _fzf_search_all() {
+        local hist=$(history | sed 's/^[ ]*[0-9]*[ ]*//')
+        local inputs=$(printf '%s\n' "$hist" "''${commons[@]}")
+        local unique=$(echo "$inputs" | tac | awk '!seen[$0]++')
+
+        READLINE_LINE=$(echo "$unique" | fzf --no-sort --prompt="Search all > ") || return
         READLINE_POINT=''${#READLINE_LINE}
       }
 
-      bind -x '"\C-p": _fzf_custom_search'
+      bind -x '"\C-p": _fzf_search_all'
     '';
   };
 }
