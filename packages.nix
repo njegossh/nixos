153a1d2a8e75ac24e187b162cbcbf2a2ea55cfcb
Marko Petrovic <markopetrovic3939@gmail.com>
2025-12-06 22:12:32 +0100

    Nikola

--- packages.nix
+++ packages.nix
@@ -2,11 +2,13 @@
   nixpkgs.config.allowUnfree = true;
   documentation.nixos.enable = false;
 
+  services.syncthing.enable = true;
+
   programs.steam.enable = true;
   environment = {
     gnome.excludePackages = [ pkgs.gnome-tour ];
     systemPackages = with pkgs; [
-      fzf nautilus foliate fragments syncthing
+      fzf nautilus foliate fragments
       debootstrap gapless gamescope blackbox-terminal
     ];
   };
--- server.nix
+++ server.nix
@@ -8,6 +8,13 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwrjcFWLvlh9ia+S2gAikdnUTUxSZajcwDTrRean/5s marko@nixos"
       ];
     };
+    nikola = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPs1Vxf1az3yuRlRSJyYBNAFr3mYdHTzRLMF70CsWhsX nikola@nikola-All-Series"
+      ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
