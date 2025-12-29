8518603250f20baa686ada354ad5f6b2dec257c3
Marko Petrovic <markopetrovic3939@gmail.com>
2025-12-29 10:34:02 +0100

    Searx

--- server.nix
+++ server.nix
@@ -22,6 +22,12 @@
     searx = {
       enable = true;
       redisCreateLocally = true;
+      settings.server = {
+        bind_address = "0.0.0.0"; 
+        port = 8888;
+        secret_key = "hehe";
+        real_ip_header = "X-Forwarded-For";
+      };
     };
     openssh = {
       enable = true;
