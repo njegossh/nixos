4f8ba3c650ee539207dea856baf24bf279ddd4de
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-02 21:10:13 +0100

    Peertube

--- server/peertube.nix
+++ server/peertube.nix
@@ -4,6 +4,8 @@
     database.createLocally = true;
     redis.createLocally = true;
     configureNginx = false;
+    listenWeb = "10.0.0.1";
+    listenHttp = "10.0.0.1";
   };
   networking.firewall.interfaces.wg0.allowedTCPPorts = [ 9000 ];
 }
