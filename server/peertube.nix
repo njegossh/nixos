053edd70ef2ac0d3e46d76002d56d7bdb012e039
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-02 21:34:10 +0100

    Peertube

--- server/peertube.nix
+++ server/peertube.nix
@@ -1,4 +1,23 @@
 { pkgs, ... } : {
+  environment.etc."peertube/secrets.env" = {
+    text = ''
+      PEERTUBE_DB_USERNAME=peertube
+      PEERTUBE_DB_PASSWORD=peertube
+      PEERTUBE_DB_HOST=127.0.0.1
+      PEERTUBE_DB_PORT=5432
+      PEERTUBE_REDIS_HOST=127.0.0.1
+      PEERTUBE_REDIS_PORT=6379
+      PEERTUBE_ADMIN_EMAIL=admin@localhost
+      PEERTUBE_SIGNUP_ENABLED=false
+      PEERTUBE_WEBSERVER_HOSTNAME=10.0.0.1
+      PEERTUBE_WEBSERVER_PORT=9000
+    '';
+    user = "peertube";
+    group = "peertube";
+  };
+  services.peertube = {
+    secrets.secretsFile = "/etc/peertube/secrets.env";
+  };
   networking.extraHosts = ''
   127.0.0.1 peertube.local
   '';
