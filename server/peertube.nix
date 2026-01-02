135d2292baf29e156c8f1d57df9f5e9ca63d0d3a
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-02 21:14:05 +0100

    Peertube

--- server/peertube.nix
+++ server/peertube.nix
@@ -1,4 +1,4 @@
-{ ... } : {
+{ pkgs, ... } : {
   services.peertube = {
     enable = true;
     database.createLocally = true;
@@ -6,6 +6,8 @@
     configureNginx = false;
     listenWeb = "10.0.0.1";
     listenHttp = "10.0.0.1";
+    localDomain = "peertube";
   };
   networking.firewall.interfaces.wg0.allowedTCPPorts = [ 9000 ];
+  environment.systemPackages = with pkgs; [ peertube ];
 }
