c4f42d36acb54428d5125cea1dd73ecdbff7af26
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-02 21:18:45 +0100

    Peertube

--- server/peertube.nix
+++ server/peertube.nix
@@ -1,4 +1,20 @@
 { ... } : {
+  environment.etc."peertube/secrets.env" = {
+    text = ''
+      PEERTUBE_DB_USERNAME=peertube
+      PEERTUBE_DB_PASSWORD=peertube
+      PEERTUBE_DB_HOST=127.0.0.1
+      PEERTUBE_DB_PORT=5432
+      PEERTUBE_REDIS_HOST=127.0.0.1
+      PEERTUBE_REDIS_PORT=6379
+      PEERTUBE_ADMIN_EMAIL=admin@localhost
+      PEERTUBE_SIGNUP_ENABLED=false
+      PEERTUBE_WEBSERVER_HOSTNAME=10.0.0.1
+      PEERTUBE_WEBSERVER_PORT=9000
+    '';
+    user = "peertube";
+    group = "peertube";
+  };
   services.peertube = {
     enable = true;
     database.createLocally = true;
@@ -7,6 +23,7 @@
     listenWeb = "10.0.0.1";
     listenHttp = "10.0.0.1";
     localDomain = "peertube";
+    secrets.secretsFile = "/etc/peertube/secrets.env";
   };
   networking.firewall.interfaces.wg0.allowedTCPPorts = [ 9000 ];
 }
