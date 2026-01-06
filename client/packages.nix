27551554717e7e3a6f84e78370815fe2637326c6
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-06 16:42:03 +0100

    Minor changes; Removed steam

--- client/packages.nix
+++ client/packages.nix
@@ -2,12 +2,11 @@
   nixpkgs.config.allowUnfree = true;
   documentation.nixos.enable = false;
   services.gnome.core-apps.enable = false;
-  programs.steam.enable = true;
   environment = {
     gnome.excludePackages = [ pkgs.gnome-tour ];
     systemPackages = with pkgs; [
-      nautilus neovim xd rclone cmus
-      gamescope blackbox-terminal
+      nautilus neovim rclone cmus epiphany
+      blackbox-terminal wine
     ];
   };
 }
