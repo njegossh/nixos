34d2bbb4fdd5b0a6f2c5fa51a12a540a422bb6f6
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-07 21:37:12 +0100

    S3

--- shared/bash.nix
+++ shared/bash.nix
@@ -16,6 +16,25 @@ in {
       PS1="[ \w ] \$\[\033[0m\] " 
       bind 'set search-ignore-case on'
       bind 'set completion-ignore-case on'
+
+
+      mount-s3() {
+          if ! ip addr show wg0 > /dev/null 2>&1; then
+              echo "Connect wireguard"
+              return 1
+          fi
+
+          read -rs "pass1?Unesi lozinku za fajlove: " && echo
+          read -rs "pass2?Unesi lozinku za imena: " && echo
+
+          rclone --config /etc/rclone.conf mount minio-enc: ~/Documents/Hetzner \
+            --vfs-cache-mode full \
+            --crypt-password "$pass1" \
+            --crypt-password2 "$pass2" \
+            --daemon
+
+          echo "✅ S3 Bucket je mount-ovan na ~/Documents/Hetzner"
+        }
     '';
   };
 }
