25764ace9b3e0f795e862c4ebb8d93d441abbf60
Marko Petrovic <markopetrovic3939@gmail.com>
2026-01-07 20:29:43 +0100

    S3

--- server/s3.nix
+++ server/s3.nix
@@ -1,12 +1,23 @@
-{ pkgs, ... } : {
-  environment.systemPackages = with pkgs; [ mc ];
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ 
+    minio-client
+  ];
+  environment.etc."minio/secrets.env" = {
+    mode = "0600";
+    text = ''
+      MINIO_ROOT_USER=admin
+      MINIO_ROOT_PASSWORD=strongpassword123
+    '';
+  };
   services.minio = {
     enable = true;
-    listenAddress = "10.0.0.1:5353";
+    listenAddress = "10.0.0.1:5353"; 
     consoleAddress = "10.0.0.1:5354";
     dataDir = [ "/var/lib/minio/data" ];
-    accessKey = "key";
-    secretKey = "key";
+    rootCredentialsFile = "/etc/minio/secrets.env";
   };
-  networking.firewall.interfaces.wg0.allowedTCPPorts = [ 5353 ];
+
+  networking.firewall.interfaces.wg0 = {
+    allowedTCPPorts = [ 5353 5354 ];
+  };
 }
