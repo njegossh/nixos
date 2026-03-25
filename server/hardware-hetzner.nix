fe39f8507297979060d77381b8e4ec0f8fd71231
Marko Petrovic <markopetrovic3939@gmail.com>
2026-03-25 22:01:16 +0100

    Removed SWAP from Hetzner

--- server/hardware-hetzner.nix
+++ server/hardware-hetzner.nix
@@ -3,12 +3,8 @@
     "/" = { device = "/dev/disk/by-label/nixos"; fsType = "ext4"; };
     "/boot" = { device = "/dev/disk/by-label/boot"; fsType = "ext4"; };
   };
-  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
   boot = {
-    loader.grub = {
-      enable = true;
-      device = "/dev/sda";
-    };
+    loader.grub = { enable = true; device = "/dev/sda"; };
     initrd.availableKernelModules = [ 
       "sd_mod" "virtio_scsi" "virtio_pci" "ahci" "xhci_pci" "ext4" "sr_mod"
     ];
